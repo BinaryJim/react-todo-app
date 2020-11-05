c96b9de802c8014d03e39c0dee9bfdd3c6db9898
James Humphreys <james@binaryjim.co.uk>
2020-11-05 15:57:46 +0000

    Added descriptions for Check prop types

--- src/components/check/check.js
+++ src/components/check/check.js
@@ -27,11 +27,29 @@ const Check = ({
 };
 
 Check.propTypes = {
+  /**
+   Adds an accessible aria-label to the check
+  */
   label: PropTypes.string.isRequired,
+  /**
+   Sets the checked attribute of the check
+  */
   checked: PropTypes.bool,
+  /**
+   Sets the disabled attribute of the check 
+  */
   disabled: PropTypes.bool,
+  /**
+   Allows passing of a callback handler to be fired on the check onchange event
+  */
   onChange: PropTypes.func.isRequired,
+  /**
+   Allows the passing of additional CSS class names to the component root node 
+  */
   classes: PropTypes.string,
+  /**
+   Allows the passing of a unique data-testid to the component root node. Used for testing purposes
+  */
   testId: PropTypes.string,
 };
 
