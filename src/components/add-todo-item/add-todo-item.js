0271d43d0a85bc7b853c7c762b622cd14f7f7550
James Humphreys <james@binaryjim.co.uk>
2020-11-06 09:43:01 +0000

    Added AddTodoItem logic to handle submission of a todo

--- src/components/add-todo-item/add-todo-item.js
+++ src/components/add-todo-item/add-todo-item.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./add-todo-item.scss";
 
-const AddTodoItem = () => {
+const AddTodoItem = ({ classes, testId }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleOnInputValueChange = (e) => {
+    setInputValue(e.target.value.trimStart());
+  };
+
+  const isValidInputValue = () => {
+    return inputValue?.length > 0 ? true : false;
+  };
+
+  const handleOnInputKeyUp = (e) => {
+    if (e.key === "Enter") {
+      handleOnSubmitTodoItem();
+    } else return;
+  };
+
+  const handleOnSubmitTodoItem = () => {
+    if (isValidInputValue()) {
+      setInputValue("");
+      console.log("Will submit todo item");
+    } else {
+      return;
+    }
+  };
+
   return (
-    <div className="add-todo-item">
+    <div className={cn("add-todo-item", classes)} data-testid={testId}>
       <input
         className="add-todo-item__input"
         type="text"
+        aria-label="Todo Item Input"
         placeholder="Type a todo..."
+        value={inputValue}
+        onChange={handleOnInputValueChange}
+        onKeyUp={handleOnInputKeyUp}
       />
-      <button className="add-todo-item__submit">
+      <button
+        className="add-todo-item__submit"
+        aria-label="Submit Todo Item"
+        disabled={!isValidInputValue()}
+        onClick={handleOnSubmitTodoItem}
+      >
         <FontAwesomeIcon
           icon={faPlus}
           className="add-todo-item__submit__icon"
