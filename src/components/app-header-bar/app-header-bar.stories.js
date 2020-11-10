1108987c940d18b59bb31657787cc48ee9fd475d
James Humphreys <james@binaryjim.co.uk>
2020-11-10 13:18:49 +0000

    Added story states for various AppHeaderBar todo counts

--- src/components/app-header-bar/app-header-bar.stories.js
+++ src/components/app-header-bar/app-header-bar.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FullWidthDecorator } from "../../../.storybook/decorators/full-width-decorator";
+import { TodoAppStateContext } from "../../store/todo-app-context";
 import { AppHeaderBar } from "./app-header-bar";
 import "../../styles/index.scss";
 
@@ -11,8 +12,51 @@ const config = {
 
 const Template = (args) => <AppHeaderBar />;
 
-const Default = Template.bind({});
-Default.args = {};
+const WithZeroTodos = Template.bind({});
+WithZeroTodos.args = {};
+WithZeroTodos.decorators = [
+  (Story) => (
+    <TodoAppStateContext.Provider
+      value={{
+        todos: [],
+      }}
+    >
+      <Story />
+    </TodoAppStateContext.Provider>
+  ),
+];
 
-export { Default };
+const WithLessThanMaxThresholdTodos = Template.bind({});
+WithLessThanMaxThresholdTodos.args = {};
+WithLessThanMaxThresholdTodos.decorators = [
+  (story) => (
+    <TodoAppStateContext.Provider
+      value={{
+        todos: [...Array(99).keys()],
+      }}
+    >
+      {story()}
+    </TodoAppStateContext.Provider>
+  ),
+];
+
+const WithGreaterThanMaxThresholdTodos = Template.bind({});
+WithGreaterThanMaxThresholdTodos.args = {};
+WithGreaterThanMaxThresholdTodos.decorators = [
+  (story) => (
+    <TodoAppStateContext.Provider
+      value={{
+        todos: [...Array(101).keys()],
+      }}
+    >
+      {story()}
+    </TodoAppStateContext.Provider>
+  ),
+];
+
+export {
+  WithZeroTodos,
+  WithLessThanMaxThresholdTodos,
+  WithGreaterThanMaxThresholdTodos,
+};
 export default config;
