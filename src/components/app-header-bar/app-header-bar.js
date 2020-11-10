12a979493847211722acc4202530c7da452025c2
James Humphreys <james@binaryjim.co.uk>
2020-11-10 13:18:20 +0000

    Added dynamic todo count to AppHeaderBar using React context hook

--- src/components/app-header-bar/app-header-bar.js
+++ src/components/app-header-bar/app-header-bar.js
@@ -1,17 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
+import { useTodoAppStateContext } from "../../store/todo-app-context";
 import "./app-header-bar.scss";
 
 const AppHeaderBar = ({ classes, testId }) => {
+  const { todos = [] } = useTodoAppStateContext();
+  const todoCount = todos.length <= 100 ? todos.length : "100+";
+
   return (
     <header
       className={cn("app-header-bar grid-container", classes)}
-      data-testId={testId}
+      data-testid={testId}
     >
       <div className="app-header-bar__content">
         <h1 className="app-header-bar__content__title">Todos</h1>
-        <span className="app-header-bar__content__todo-count">0</span>
+        <span className="app-header-bar__content__todo-count">{todoCount}</span>
       </div>
     </header>
   );
