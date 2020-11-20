51536e5e8908c9f61ae28450d3f28c458dd38bd8
James Humphreys <james@binaryjim.co.uk>
2020-11-20 11:38:07 +0000

    #23: Added TodosFilter component markup, styles and story

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { AppHeaderBar } from "./app-header-bar";
 export { AddTodoItem } from "./add-todo-item";
 export { TodoItem } from "./todo-item";
 export { TodoItems } from "./todo-items";
+export { TodosFilter } from "./todos-filter";
