698ed0570ad2203ccfe462cae8352dda22011280
James Humphreys <james@binaryjim.co.uk>
2020-12-09 10:53:29 +0000

    #11 Added tests for MainView component

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { AppHeaderBar } from "../app-header-bar";
 import { AddTodoItem } from "../add-todo-item";
 import { TodoItems } from "../todo-items";
---
+++ src/components/main-view/main-view.test.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MainView } from "./main-view";
+import { TodoAppStore } from "../../store/todo-app-store";
+
+const state = {
+  filter: "ALL",
+  todos: [
+    {
+      id: 1,
+      description: "Test todo item 1",
+      pinned: false,
+      done: false,
+    },
+    {
+      id: 2,
+      description: "Test todo item 2",
+      pinned: true,
+      done: false,
+    },
+    {
+      id: 3,
+      description: "Test todo item 3",
+      pinned: false,
+      done: true,
+    },
+  ],
+};
+
+const MockProvider = ({ children, state }) => {
+  return (
+    <TodoAppStore.Provider
+      value={{
+        state,
+        actions: {
+          toggleTaskStatus: () => {},
+          toggleTaskPin: () => {},
+          removeTodo: () => {},
+        },
+      }}
+    >
+      {children}
+    </TodoAppStore.Provider>
+  );
+};
+
+describe("MainView - with todos", () => {
+  describe("with todos filtered by all todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={state}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  describe("with todos filtered by open todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={{ ...state, filter: "OPEN" }}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  describe("with todos filtered by done todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={{ ...state, filter: "DONE" }}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
+
+describe("MainView - with no todos", () => {
+  describe("with todos filtered by all todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={{ filter: "ALL", todos: [] }}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  describe("with todos filtered by open todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={{ filter: "OPEN", todos: [] }}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  describe("with todos filtered by done todos", () => {
+    it("should match snapshot", () => {
+      const { asFragment } = render(
+        <MockProvider state={{ filter: "DONE", todos: [] }}>
+          <MainView />
+        </MockProvider>
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
